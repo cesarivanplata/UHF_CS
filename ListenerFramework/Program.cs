72671280411a36f957addac3a3697286a4551200
Cesar Plata <ciplatacolin@kzsolution.com.mx>
2022-03-08 16:07:37 -0600

    Estadarizado

--- ListenerFramework/Program.cs
+++ ListenerFramework/Program.cs
@@ -15,22 +15,22 @@ namespace ListenerFramework
         static void Main(string[] args)
         {
             int socket = 100;
-            string aipi = "192.168.1.201";
+            string aipi = "192.168.1.200";
             ReaderModel antena1 = new ReaderModel(aipi, socket);
-            ReaderModel antena2 = new ReaderModel("192.168.1.200", 100);
-            Thread hiloLeer = new Thread(() => antena1.readIDs_both());
-            Thread hiloEscribir = new Thread(() => antena1.getstream_IDs());
-            Thread hiloLeer2 = new Thread(() => antena2.readIDs_both());
-            Thread hiloEscribir2 = new Thread(() => antena2.getstream_IDs());
-            Console.WriteLine(antena1.conectar());
-            Console.WriteLine(antena2.conectar());
+            ReaderModel antena2 = new ReaderModel("192.168.1.201", 100);
+            Thread hiloLeer = new Thread(() => antena1.ReadIDsBoth());
+            Thread hiloEscribir = new Thread(() => antena1.GetStreamIDs());
+            Thread hiloLeer2 = new Thread(() => antena2.ReadIDsBoth());
+            Thread hiloEscribir2 = new Thread(() => antena2.GetStreamIDs());
+            Console.WriteLine(antena1.Conectar());
+            Console.WriteLine(antena2.Conectar());
             hiloLeer.Start();
             hiloLeer2.Start();
             hiloEscribir.Start();
             hiloEscribir2.Start();
             string a = Console.ReadLine();
-            antena1.desconectar();
-            antena2.desconectar();
+            antena1.Desconectar();
+            antena2.Desconectar();
             Console.WriteLine("Termina la lectura de Tags");
             Console.WriteLine("Presione cualquier tecla");
             a = Console.ReadLine();
